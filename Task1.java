0cd2871f25003a81a325364f00afdcffdf29035a
George <madeinerussia@yandex.ru>
2023-06-17 23:27:53 +0500

    Changed Task2

--- Task1.java
+++ Task1.java
@@ -2,6 +2,10 @@ public class Task1 {
     /**
      * @param args
      */
+//     Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// Пример вывода: "select * from students WHERE name = Ivanov AND country = Russia.....".
     public static void main(String[] args) {
         String str = "name:Ivanov, country:Russia, city:Moscow, age:null";
         System.out.println(str);
@@ -9,6 +13,7 @@ public class Task1 {
         StringBuilder result = new StringBuilder("select * from students where ");
 
         String [] arrayData1 = str.split(", ");
+
         for (int i =0; i < arrayData1.length; i++) {
             String[] arrayData2 = arrayData1[i].split(":");
             if(arrayData2[1].equals("null") == false) {
